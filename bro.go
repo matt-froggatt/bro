8f492908ce3121253161d7844c9669ddf93a548a
Sunny Xie <sunnyxie0@gmail.com>
2019-11-09 14:03:23 -0500

    Basic server code.

--- bro.go
+++ bro.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	"net/http"
+	"github.com/gorilla/mux"
 	"log"
-	//""
+	"net/http"
 )
 
 func main() {
+	router := mux.NewRouter()
+	buildHandler := http.FileServer(http.Dir("frontend/build"))
+	router.PathPrefix("/").Handler(buildHandler)
+	staticHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("frontend/build/static")))
+	router.PathPrefix("/static/").Handler(staticHandler)
 
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
 }
