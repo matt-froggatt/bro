abb0402499889511c3e74ae37c3f60d5e57b9d3d
Matthew Froggatt <mattfr@live.ca>
2019-11-09 14:49:19 -0500

    Finally figured out create-react-app.

--- bro.go
+++ bro.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
-	if _, err := fmt.Fprintf(w, "Hello World") ; err != nil {
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	if _, err := fmt.Fprintf(w, "Hello World"); err != nil {
 		log.Fatalln(err)
 	}
 }
 
 func main() {
 	router := mux.NewRouter()
-	buildHandler := http.FileServer(http.Dir("frontend/"))
+	buildHandler := http.FileServer(http.Dir("frontend/build"))
 	router.PathPrefix("/").Handler(buildHandler)
 
 	http.HandleFunc("/", helloWorld)
